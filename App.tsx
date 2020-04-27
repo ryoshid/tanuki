3eb87594ed928dffc6fd29036f170874e2982d1d
reedry <r.yoshida.c@gmail.com>
2020-04-27 21:58:51 +0900

    Add function to format duration (ms)

--- App.tsx
+++ App.tsx
@@ -5,13 +5,19 @@ const MS_S = 1000;
 const MS_M = MS_S * 60;
 const MS_H = MS_M * 60;
 
+const formatDuration = (ms: number): string => {
+  const hour = Math.floor(ms / MS_H);
+  const minute = Math.floor((ms - hour * MS_H) / MS_M);
+  const second = Math.floor((ms - (hour * MS_H + minute * MS_M)) / MS_S);
+  return [hour, minute, second]
+    .map((n) => String(n).padStart(2, "0"))
+    .join(":");
+};
+
 const timeDiff = (from: Date, to: Date): string => {
-  if (from.getTime() > to.getTime()) return "0:0:0";
+  if (from.getTime() > to.getTime()) return formatDuration(0);
   const diff = to.getTime() - from.getTime();
-  const hour = Math.floor(diff / MS_H);
-  const minute = Math.floor((diff - hour * MS_H) / MS_M);
-  const second = Math.floor((diff - (hour * MS_H + minute * MS_M)) / MS_S);
-  return `${hour}:${minute}:${second}`;
+  return formatDuration(diff);
 };
 
 export default function App() {
