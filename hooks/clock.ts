06faf19b81e30fe834d46e3eae454f1ce9f1fab2
reedry <r.yoshida.c@gmail.com>
2020-04-28 01:00:33 +0900

    Add Hooks for clock

---
+++ hooks/clock.ts
@@ -0,0 +1,10 @@
+import { useEffect, useState } from "react";
+
+export const useClock = (): Date => {
+  const [now, setNow] = useState(new Date());
+  useEffect(() => {
+    const timer = setInterval(() => setNow(new Date()), 1000);
+    return () => clearInterval(timer);
+  }, []);
+  return now;
+};
