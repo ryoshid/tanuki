d1daa53deccd458ad2f0b4a1839cf89cf46827de
reedry <r.yoshida.c@gmail.com>
2020-04-28 23:46:14 +0900

    Move time-related functions

--- App.tsx
+++ App.tsx
@@ -2,25 +2,7 @@ import React from "react";
 import { Button, StyleSheet, Text, View } from "react-native";
 import { usePersistedState } from "./hooks/persistedState";
 import { useClock } from "./hooks/clock";
-
-const MS_S = 1000;
-const MS_M = MS_S * 60;
-const MS_H = MS_M * 60;
-
-const formatDuration = (ms: number): string => {
-  const hour = Math.floor(ms / MS_H);
-  const minute = Math.floor((ms - hour * MS_H) / MS_M);
-  const second = Math.floor((ms - (hour * MS_H + minute * MS_M)) / MS_S);
-  return [hour, minute, second]
-    .map((n) => String(n).padStart(2, "0"))
-    .join(":");
-};
-
-const timeDiff = (from: Date, to: Date): string => {
-  if (from.getTime() > to.getTime()) return formatDuration(0);
-  const diff = to.getTime() - from.getTime();
-  return formatDuration(diff);
-};
+import { timeDiff } from "./util/time";
 
 const App: React.FC = () => {
   const [pressedTimeMS, setPressedTime] = usePersistedState<undefined | number>(
