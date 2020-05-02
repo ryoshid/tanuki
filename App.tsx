3d1a2bf591187bab5d6221b8d42da36cf0137616
ryoshid <r.yoshida.c@gmail.com>
2020-05-02 23:30:54 +0900

    Add several events

--- App.tsx
+++ App.tsx
@@ -4,21 +4,33 @@ import { usePersistedState } from "./hooks/persistedState";
 import { useClock } from "./hooks/clock";
 import { timeDiff } from "./util/time";
 
+type EventKind = "Start" | "GoToBed" | "TryToSleep" | "WakeUp" | "GetUp";
+
+const nextEventName: { [K in EventKind]: [EventKind] } = {
+  Start: ["GoToBed"],
+  GoToBed: ["TryToSleep"],
+  TryToSleep: ["WakeUp"],
+  WakeUp: ["GetUp"],
+  GetUp: ["GetUp"],
+};
+
 type Event = {
-  name: string;
+  name: EventKind;
   time_ms: {
     from: number | undefined;
     to: number | undefined;
   };
   pressed: boolean;
+  nextEvents: EventKind[];
 };
 
 type Events = Event[];
 
 const defaultEvent: Event = {
-  name: "test",
+  name: "Start",
   time_ms: { from: undefined, to: undefined },
   pressed: false,
+  nextEvents: ["GoToBed"],
 };
 
 const App: React.FC = () => {
@@ -29,25 +41,27 @@ const App: React.FC = () => {
   const [events, setEvents] = usePersistedState("events", [defaultEvent]);
   const pressedTime = new Date(pressedTimeMS ?? 0);
   const now = useClock();
-  const lastEvent = events[events.length - 1];
+  const currentEvent = events[events.length - 1];
   const handlePress = () => {
     setPressedTime(new Date().getTime());
   };
-  const handleEventPress = () => {
+  const handleEventPress = (nextEvent: EventKind) => {
     setEvents((prev) => {
       console.log(prev);
-      const from = prev[prev.length - 1].time_ms.from;
+      const lastEvent = prev[prev.length - 1];
       const now_ms = new Date().getTime();
       return prev
         .slice(0, prev.length - 1)
         .concat({
-          ...defaultEvent,
+          ...lastEvent,
           pressed: true,
-          time_ms: { from, to: now_ms },
+          time_ms: { from: lastEvent.time_ms.from, to: now_ms },
         })
         .concat({
           ...defaultEvent,
+          name: nextEvent,
           time_ms: { from: now_ms, to: undefined },
+          nextEvents: nextEventName[nextEvent],
         });
     });
   };
@@ -62,22 +76,28 @@ const App: React.FC = () => {
       />
       {events.map((ev, i) => (
         <View key={i}>
-          {!ev.pressed ? (
-            <Button title="Press" onPress={() => handleEventPress()} />
-          ) : (
-            ev.time_ms.from &&
-            ev.time_ms.to && (
-              <Text>
-                {timeDiff(new Date(ev.time_ms.from), new Date(ev.time_ms.to))}
-                since pressed
-              </Text>
-            )
-          )}
+          {!ev.pressed
+            ? ev.nextEvents.map((evName, j) => (
+                <Button
+                  key={j}
+                  title={evName}
+                  onPress={() => handleEventPress(evName)}
+                />
+              ))
+            : ev.time_ms.from &&
+              ev.time_ms.to && (
+                <Text>
+                  {`${ev.name}: ${timeDiff(
+                    new Date(ev.time_ms.from),
+                    new Date(ev.time_ms.to)
+                  )}`}
+                </Text>
+              )}
         </View>
       ))}
-      {lastEvent.time_ms.from && (
+      {currentEvent.time_ms.from && (
         <Text>
-          Time elapsed: {timeDiff(new Date(lastEvent.time_ms.from), now)}
+          Time elapsed: {timeDiff(new Date(currentEvent.time_ms.from), now)}
         </Text>
       )}
       <Button title="Press Me" onPress={handlePress} />
